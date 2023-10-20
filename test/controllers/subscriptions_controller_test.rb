cf60fb1b067a50987374dcc745dc57e785fae191
Talha Nasir <talha.nasir@gmail.com>
2023-10-20 15:27:34 +0500

    Add test controllers and policies

---
+++ test/controllers/subscriptions_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SubscriptionsControllerTest < ActionDispatch::IntegrationTest
+  test 'should get create' do
+    get subscriptions_create_url
+    assert_response :success
+  end
+end
